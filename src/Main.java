cf201999ef459c65c312b59c88690278c6ce47f5
kregoric <75627074+isicisawsum@users.noreply.github.com>
2024-02-15 17:33:12 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,86 @@
+import java.util.Scanner; //importing Scanner
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner in = new Scanner(System.in); //naming Scanner
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        boolean loop = true; //ability for question to loop if user inputs wrong
+
+        double numGas = 0;
+
+        double MPG = 0;
+
+        double gas = 0;
+
+        System.out.println("What is the number of gallons of gas in your tank?"); //asking user for variable numGas
+
+        do {
+            if (in.hasNextDouble()) { //testing if user entered a number
+
+                numGas = in.nextDouble(); //calculating variable numGas to user input
+
+                loop = false; //turning off this loop
+
+            }
+            else if (!in.hasNextDouble()) {
+
+                String trash = in.nextLine(); //user entered something wrong
+
+                System.out.println(trash + " is not a number"); //telling user they entered something wrong
+
+            }
+        } while(loop); //loops while 'loop' = true
+
+        System.out.println("What is the fuel efficiency in miles/gallon? (Just type the number)"); //asking user for variable MPG
+
+        loop = true; //turning loop back on
+
+        do {
+            if (in.hasNextDouble()) { //testing if user entered a number
+
+                MPG = in.nextDouble(); //calculating variable MPG to user input
+
+                loop = false; //turning off this loop
+
+            }
+            else if (!in.hasNextDouble()) {
+
+                String trash = in.nextLine(); //user entered something wrong
+
+                System.out.println(trash + " is not a number"); //telling user they entered something wrong
+
+            }
+        } while(loop); //loops while 'loop' = true
+
+        System.out.println("What is price of gas per gallon? (Just enter the number)"); //asking user for variable gas
+
+        loop = true; //turning loop back on
+
+        do {
+            if (in.hasNextDouble()) { //testing if user entered a number
+
+                gas = in.nextDouble(); //calculating variable miles to user input
+
+                loop = false; //turning off this loop
+
+            }
+            else if (!in.hasNextDouble()) {
+
+                String trash = in.nextLine(); //user entered something wrong
+
+                System.out.println(trash + " is not a number"); //telling user they entered something wrong
+
+            }
+        } while(loop); //loops while 'loop' = true
+
+        double far = (numGas * MPG); //calculating how far they can go
+
+        double totPrice = ((100/MPG) * gas); //calculating the total price
+
+        System.out.println("The cost to go 100 miles is $" + totPrice);
+
+        System.out.println("The car can go " + far + " miles"); //telling user both things
+
     }
 }
